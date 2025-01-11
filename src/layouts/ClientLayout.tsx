6d853d97be3717f89d643f13db3eefd02f0f2852
Harsh nath <you@example.com>
2025-01-12 02:59:44 +0530

    aboutchange

--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -5,22 +5,22 @@ import { Footer } from '../components/common/Footer';
 import LocomotiveScroll from 'locomotive-scroll';
 
 const ClientLayout = (props: any) => {
-  const containerRef = useRef<any>(null);;
-  useEffect(() => {
-    // Initialize Locomotive Scroll
-    const scrollInstance = new LocomotiveScroll({
-      el: containerRef.current,
-      smooth: true,
-      lerp: 0.5, 
-    });
+  // const containerRef = useRef<any>(null);;
+  // useEffect(() => {
+  //   // Initialize Locomotive Scroll
+  //   const scrollInstance = new LocomotiveScroll({
+  //     el: containerRef.current,
+  //     smooth: true,
+  //     lerp: 0.5, 
+  //   });
 
-    return () => {
-      // Clean up Locomotive Scroll instance
-      scrollInstance.destroy();
-    };
-  }, []);
+  //   return () => {
+  //     // Clean up Locomotive Scroll instance
+  //     scrollInstance.destroy();
+  //   };
+  // }, []);
   return (
-    <div ref={containerRef} data-scroll-container>
+    <div>
       <Container fluid className='p-0'>
         <Header />
         {props.children}
