96a634e7ad1703ede0318c993667f4e174a1e756
ajays <ajays@capitalnumbers.com>
2025-01-24 02:06:56 +0530

    'maps'

--- src/pages/client/Contact.tsx
+++ src/pages/client/Contact.tsx
@@ -1,88 +1,105 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import Map, { Layer, Source } from 'react-map-gl/maplibre';
 import CustomBreadcrum from "../../components/breadcrum/Breadcrum";
 import contact from '../../assets/contact/contact.svg'
+import { useState } from 'react';
+import "mapbox-gl/dist/mapbox-gl.css"; // Add Mapbox CSS
 
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  // iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  // iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  // shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-});
+
 export const Contact = () => {
 
-  const position: [number, number] = [51.505, -0.09]; // Default coordinates (London)
-  // Fix default icon issue with Leaflet
+  const sourceId = "places";
+  const circleLayerId = "circles";
+  const symbolLayerId = "symbols";
+  const [circleLayerRadius, setCircleLayerRadius] = useState(15);
+  // const [circleLayerVisibility, setCircleLayerVisibility] = useState("visible");
+  // const [symbolLayerVisibility, setSymbolLayerVisibility] = useState("visible");
 
+  // const handleCircleLayerRadius = (radius: number) => {
+  //   setCircleLayerRadius(radius);
+  // };
+  // const handleCircleLayerVisibility = (checked: boolean) => {
+  //   setCircleLayerVisibility(checked ? "visible" : "none");
+  // };
+  // const handleSymbolLayerVisibility = (checked: boolean) => {
+  //   setSymbolLayerVisibility(checked ? "visible" : "none");
+  // };
   return (
     <div className="margin_70">
       <CustomBreadcrum title={'CONTACT US'} baseLine={'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, officiis.'} image={contact} />
 
-      <div className='contact_bg'>
+      {/* <div className='contact_bg'>
         <h1 className='text-center'>In case you need to find us.<br />
           These are the coordinates for your biggest adventure yet.</h1>
-      </div>
-      {/* <Container> */}
-      <div >
-        {/* <MapContainer
-          center={position}
-          zoom={13}
-          style={{ height: "400px", width: "100%" }}
+      </div> */}
+      <div className='py-5'>
+
+        <Map
+          reuseMaps
+          dragRotate={false}
+          touchZoomRotate={false}
+          initialViewState={{
+            longitude: -122.4,
+            latitude: 37.8,
+            zoom: 1,
+          }}
+          style={{ width: '100%', height: '600px' }}
+          mapStyle="https://api.maptiler.com/maps/streets/style.json?key=fWe6ey4q0sUTydAJSk3E"
+
         >
-          {/* TileLayer defines the map appearance */}
-        {/* <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          /> */}
-
-        {/* Example marker */}
-        {/* <Marker position={position}>
-            <Popup>
-              A marker in London. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer> */}
-        {/* <MapContainer center={{ lat: 51.505, lng: -0.09 }}
-            zoom={zoom}
-            scrollWheelZoom={true}
-            // ref={setMap}
-            style={{ width: "500", height: "500px" }}>
-            <h1 className='text-center'>Find us anywhere, because we’re putting ourselves on the map!</h1>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          <Source
+            id={sourceId}
+            type="geojson"
+            maxzoom={5}
+            data="https://maplibre.org/maplibre-gl-js/docs/assets/earthquakes.geojson"
+          >
+            <Layer
+              {...{
+                source: sourceId,
+                id: circleLayerId,
+                type: "circle" as const,
+                paint: {
+                  "circle-color": "#ff0000",
+                  "circle-radius": circleLayerRadius,
+                  "circle-stroke-width": 0,
+                  "circle-stroke-color": "#000",
+                },
+                layout: {
+                  // visibility: circleLayerVisibility,
+                },
+              }}
             />
-            <Marker
-              position={position}
-              icon={icon}
-            >
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer> */}
-
-        {/* <MapContainer center={position} zoom={zoom} style={{ height: '400px', width: '100%' }}>
-          {/* <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          /> */}
-        {/* <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker> */}
-        {/* </MapContainer> */}
+            <Layer
+              {...{
+                id: symbolLayerId,
+                type: "symbol" as const,
+                source: sourceId,
+                layout: {
+                  // visibility: symbolLayerVisibility,
+                  "text-allow-overlap": true,
+                  "text-font": ["Arial Italic"],
+                  "text-field": ["get", "mag"],
+                  "text-size": [
+                    "interpolate",
+                    ["linear"],
+                    ["zoom"],
+                    // zoom is 5 (or less)
+                    5,
+                    12,
+                    // zoom is 10 (or greater)
+                    10,
+                    11,
+                  ],
+                  "text-anchor": "center",
+                  // "text-offset": [0, -2],
+                },
+              }}
+            />
+          </Source>
+        </Map>
       </div>
-      {/* </Container> */}
-      {/* <div className='contact_bg'> </div>
-      <div className='contact_bg'> </div>
-      <div className='contact_bg'> </div>
-      <div className='contact_bg'> </div>
-      <div className='contact_bg'> </div>
-      <div className='contact_bg'> </div> */}
 
     </div>
   )
 }
+
+
